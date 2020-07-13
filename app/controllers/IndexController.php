ac245f6272c1a70a3ef29d665879fbd3db73c699
TherTale <68145652+TherTale@users.noreply.github.com>
2020-07-14 01:15:52 +0400

    SigUP

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,6 +8,25 @@ class IndexController extends ControllerBase
     {
 
     }
+    public function signinAction()
+    {
+    	if ($this->request->isPost()) {
+    	$mail = $this->request->getPost('email');
+    	$pass = $this->request->getPost('password');
+
+    	if($mail != "" && $pass!=""){
+    		echo "Успешный вход, временно так, потом будет лучше";
+    	}
+    	else if($mail == ""){
+    		echo "Почта не введена";
+    	}
+    	else if ($pass == "") {
+    		echo "<br>Пароль не введен";
+    	}
+    }
+
+
+    }
 
 }
 
