1a9776a85d013ab1fa940d518748519fd39251b0
UnhappyF <40140979+UnhappyF@users.noreply.github.com>
2020-07-17 14:57:03 +0400

    Заготовка

--- app/controllers/PhonebookController.php
+++ app/controllers/PhonebookController.php
@@ -1,17 +1,40 @@
 <?php
 declare(strict_types=1);
+use Phalcon\Mvc\Model\Criteria;
+use Phalcon\Paginator\Adapter\Model as Paginator;
+
 
 class PhonebookController extends ControllerBase
 {
 
     public function indexAction()
     {
-
+	
     }
   
 	public function searchAction()
 	{
 
+		$query = Criteria::fromInput($this->di, "People", $this->request->getPost());
+		
+		
+		$people = People::find($query->getParams());
+if (count($people) == 0) {
+$this->flash->notice("Поиск не нашел никаких продуктов");
+return $this->forward("phonebook/index");
+}
+
+		
+		$paginator = new Paginator(
+array(
+"data" => $people, // Данные для пагинации
+"limit" => 5, // Количество записей на страницу
+"page" => 1 // Активная страница
+)
+);
+
+		$page = $paginator->Paginate();
+		$this->view->page = $page;
 	}
 
 	public function newAction()
