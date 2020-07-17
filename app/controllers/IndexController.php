218cc891cb2dfa47b69cd235afc99dfa41e454aa
TherTale <68145652+TherTale@users.noreply.github.com>
2020-07-17 14:38:41 +0400

    signIN
    Переделал авторизацию пользователя, подгатавливаю к тому, что бы настроить доступ у зарегестрированых пользователей

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,11 +8,24 @@ class IndexController extends ControllerBase
     {
 
     }
+
+    private function _registerSession($user)
+    {
+        $this->session->set(
+            'auth',
+            [
+                'id'   => $user->id,
+                'name' => $user->name,
+            ]
+        );
+    }
+
     public function signinAction()
     {
-        $mail = $this->request->getPost('email');
-        $pass = $this->request->getPost('password');    
-        $user = User::find(
+        if($this->request->isPost()){
+            $mail = $this->request->getPost('email');
+            $pass = $this->request->getPost('password');  
+            $user = User::findFirst(
             [
                 'email=:mail: AND password = :pass:',
                 'bind'=>[
@@ -22,19 +35,34 @@ class IndexController extends ControllerBase
 
             ]
         );
-        if($mail == "" || $pass == ""){
-            echo "Почта или пароль не введены";
-        }
-        else if($user->count() ==1)
-        {
-            echo $user->count();
-           # header("Location: /phonebook");
-        }
-        else
-        {
-            echo "Неправильный логин или пароль";
-        }
-    }
+        if ($user !== null) {
+            $this->_registerSession($user);
 
+            $this->flash->success(
+            'Добро пожаловать ' . $user->name
+            );
+            #Если юзер прошел авторизацию, то пропускаем его к контактам
+            return $this->dispatcher->forward(
+                    [
+                        'controller' => 'phonebook',
+                        'action'     => 'index',
+                    ]
+                );
+            }
+            $this->flash->error(
+                'Неправильный пароль или Email адрес'
+            );
+        }
+        #Пользователь не смог пройти авторизацию, выводим ему эту же страницу
+        return $this->dispatcher->forward(
+            [
+                'controller' => 'index',
+                'action'     => 'index',
+            ]
+        );
+    }
 }
+
+
+
 
